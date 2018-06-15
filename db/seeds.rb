b642450145ab796cffa8d79360812fc8438adce4
Jonas Morgenthaler <jonas.n.morgenthaler@gmail.com>
2018-06-15 11:09:00 +0200

    done but no styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.delete_all
+
+ingredients = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+
+puts "Beginning seed..."
+i = 0
+JSON.parse(ingredients).each do |ingredi|
+ ingredi[1].each do |ingredient|
+ i += 1
+   @item = ingredient
+
+     Ingredient.create ({
+       "name":  @item["strIngredient1"]
+     })
+ puts "Ingredient \##{i} added."
+ puts "#{ingredient}"
+end
+end
+
+puts "Seed end"
